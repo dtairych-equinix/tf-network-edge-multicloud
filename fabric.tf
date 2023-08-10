4f9370a1be908da8ae6d13b901e64d66bec0a26a
dtairych-equinix <dtairych@ap.equinix.com>
2023-08-10 11:24:51 +0200

    Move user notification to list in vars
    Signed-off-by: dtairych-equinix <dtairych@ap.equinix.com>

--- fabric.tf
+++ fabric.tf
@@ -9,7 +9,7 @@ resource "equinix_ecx_l2_connection" "clouds" {
   profile_uuid      = data.equinix_ecx_l2_sellerprofile.clouds[each.key].uuid
   speed             = each.value.speed
   speed_unit        = "MB"
-  notifications     = var.equinix_fabric_user
+  notifications     = var.equinix_fabric_users
   device_uuid         = equinix_network_device.multi-cloud-router.id
   device_interface_id = each.value.ne_interface
   seller_metro_code = var.network_edge_metro
