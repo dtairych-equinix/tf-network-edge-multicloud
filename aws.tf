e509e7715a2a11aa81469f49a6069df570b3dce6
dtairych-equinix <dtairych@ap.equinix.com>
2023-08-24 22:38:34 +0200

    updated AWS network from vars

--- aws.tf
+++ aws.tf
@@ -11,8 +11,8 @@ resource "aws_dx_private_virtual_interface" "multicloud" {
   name             = "private-vif"
   vlan             = equinix_ecx_l2_connection.clouds["AWS"].zside_vlan_stag
   address_family   = "ipv4"
-  amazon_address   = "${cidrhost(var.aws_interconnect_network, 2)}/30"
-  customer_address = "${cidrhost(var.aws_interconnect_network, 1)}/30"
+  amazon_address   = "${cidrhost(var.clouds["AWS"].interconnect_network, 2)}/30"
+  customer_address = "${cidrhost(var.clouds["AWS"].interconnect_network, 1)}/30"
   bgp_asn          = 65000
   dx_gateway_id = data.aws_dx_gateway.multicloud.id
   bgp_auth_key     = var.clouds["AWS"].bgp_password
